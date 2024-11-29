74b61a9f47ecbc653f24d32a058bdb8ddb10022c
Reejh Ghosh <reejh.ghosh@sentinelone.com>
2024-11-29 15:11:18 +0530

    feature: add authentication support

--- proxy-server.js
+++ proxy-server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const gremlin = require('gremlin');
@@ -6,49 +7,53 @@ const path = require('path');
 const app = express();
 const port = 3001;
 
+const username = process.env.USERNAME || "";
+const password = process.env.PASSWORD || "";
+const protocol = process.env.PROTOCOL || "";
+
 app.use(cors({
-  credentials: true,
+    credentials: true,
 }));
 
 // parse application/json
 app.use(bodyParser.json());
 
 function mapToObj(inputMap) {
-  let obj = {};
+    let obj = {};
 
-  inputMap.forEach((value, key) => {
-    obj[key] = value
-  });
+    inputMap.forEach((value, key) => {
+        obj[key] = value
+    });
 
-  return obj;
+    return obj;
 }
 
 function edgesToJson(edgeList) {
-  return edgeList.map(
-    edge => ({
-      id: typeof edge.get('id') !== "string" ? JSON.stringify(edge.get('id')) : edge.get('id'),
-      from: edge.get('from'),
-      to: edge.get('to'),
-      label: edge.get('label'),
-      properties: mapToObj(edge.get('properties')),
-    })
-  );
+    return edgeList.map(
+        edge => ({
+            id: typeof edge.get('id') !== "string" ? JSON.stringify(edge.get('id')) : edge.get('id'),
+            from: edge.get('from'),
+            to: edge.get('to'),
+            label: edge.get('label'),
+            properties: mapToObj(edge.get('properties')),
+        })
+    );
 }
 
 function nodesToJson(nodeList) {
-  return nodeList.map(
-    node => ({
-      id: node.get('id'),
-      label: node.get('label'),
-      properties: mapToObj(node.get('properties')),
-      edges: edgesToJson(node.get('edges'))
-    })
-  );
+    return nodeList.map(
+        node => ({
+            id: node.get('id'),
+            label: node.get('label'),
+            properties: mapToObj(node.get('properties')),
+            edges: edgesToJson(node.get('edges'))
+        })
+    );
 }
 
 function makeQuery(query, nodeLimit) {
-  const nodeLimitQuery = !isNaN(nodeLimit) && Number(nodeLimit) > 0 ? `.limit(${nodeLimit})`: '';
-  return `${query}${nodeLimitQuery}
+    const nodeLimitQuery = !isNaN(nodeLimit) && Number(nodeLimit) > 0 ? `.limit(${nodeLimit})` : '';
+    return `${query}${nodeLimitQuery}
   .dedup()
   .as('node')
   .project('id', 'label', 'properties', 'edges')
@@ -67,44 +72,59 @@ function makeQuery(query, nodeLimit) {
 }
 
 app.post('/query', (req, res, next) => {
-  const gremlinHost = req.body.host;
-  const gremlinPort = req.body.port;
-  const nodeLimit = req.body.nodeLimit;
-  const query = req.body.query;
-  const traversalSource = req.body.traversalSource;
+    const gremlinHost = req.body.host;
+    const gremlinPort = req.body.port;
+    const nodeLimit = req.body.nodeLimit;
+    const query = req.body.query;
+    const traversalSource = req.body.traversalSource;
 
-  const client = new gremlin.driver.Client(`ws://${gremlinHost}:${gremlinPort}/gremlin`, { traversalSource: traversalSource, mimeType: 'application/json' });
 
-  client.submit(makeQuery(query, nodeLimit), {})
-    .then((result) => res.send(nodesToJson(result._items)))
-    .catch((err) => next(err));
+    let defaultOptions = {
+        traversalSource: traversalSource, mimeType: 'application/json',
+        rejectUnauthorized: false
+    }
+
+    if (username.length && password.length) {
+        defaultOptions.authenticator = new gremlin.driver.auth.PlainTextSaslAuthenticator(username, password);
+    }
+
+    let defaultConnectionString = `ws://${gremlinHost}:${gremlinPort}/gremlin`;
+    if (protocol.length) {
+        defaultConnectionString = `${protocol}://${gremlinHost}:${gremlinPort}/gremlin`
+    }
+
+    const client = new gremlin.driver.Client(defaultConnectionString, defaultOptions);
+
+    client.submit(makeQuery(query, nodeLimit), {})
+        .then((result) => res.send(nodesToJson(result._items)))
+        .catch((err) => next(err));
 
 });
 
 app.get('/settings', (_, res) => {
-  return res.json({
-    GREMLIN_HOST: firstNotNull(process.env.GREMLIN_HOST, 'localhost'),
-    GREMLIN_PORT: firstNotNull(process.env.GREMLIN_PORT, '8182'),
-    GREMLIN_TRAVERSAL_SOURCE: firstNotNull(process.env.GREMLIN_TRAVERSAL_SOURCE, 'g'),
-    GREMLIN_DEFAULT_QUERY: firstNotNull(process.env.GREMLIN_DEFAULT_QUERY, 'g.V()'),
-  });
+    return res.json({
+        GREMLIN_HOST: firstNotNull(process.env.GREMLIN_HOST, 'localhost'),
+        GREMLIN_PORT: firstNotNull(process.env.GREMLIN_PORT, '8182'),
+        GREMLIN_TRAVERSAL_SOURCE: firstNotNull(process.env.GREMLIN_TRAVERSAL_SOURCE, 'g'),
+        GREMLIN_DEFAULT_QUERY: firstNotNull(process.env.GREMLIN_DEFAULT_QUERY, 'g.V()'),
+    });
 });
 
 // Hosting react app in express 
 // https://create-react-app.dev/docs/deployment#other-solutions
-app.use(express.static(path.join(__dirname, 'frontend')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (_, res) {
-  res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(port, () => console.log(`Simple gremlin-proxy server listening on port ${port}!`));
 
 function firstNotNull() {
-  for (let i = 0; i < arguments.length; i++) {
-    const arg = arguments[i];
-    if (arg) {
-      return arg;
+    for (let i = 0; i < arguments.length; i++) {
+        const arg = arguments[i];
+        if (arg) {
+            return arg;
+        }
     }
-  }
 }
